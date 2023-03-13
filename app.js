a4bc2c5e2784738245142620c959a00adfeb1d9c
Omolara Adebowale <debbiegterra@gmail.com>
2023-03-13 18:11:47 +0100

    chore: :construction: Connect server to Database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const errorMiddleware = require('./middleware/error-handler');
 const notFoundMiddleware = require('./middleware/not-found');
+const connectDB = require('./db/connect')
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.use(errorMiddleware);
 const PORT = process.env.PORT || 4002;
 const startServer = async () => {
     try {
+        //Connect to Database
+        await connectDB(process.env.MONGODB_URI);
         app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
     } catch (error) {
         console.log(error);
