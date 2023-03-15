91b50f612afc7ce18831ead3d9e14a39f99ab3cd
Omolara Adebowale <debbiegterra@gmail.com>
2023-03-15 21:29:43 +0100

    chore: :tada: Console log filters to see regex mapper is working

--- controllers/products.js
+++ controllers/products.js
@@ -41,11 +41,12 @@ const getAllProducts = async (req, res) => {
     }
     const regEx = /\b(> | >= | = | < | <=)\b/g;
     let filters = numericFilters.replace(regEx, match = `-${operatorsMap[match]}-`);
+    console.log(filters);
     const options = ['price', 'rating'];
     filters = filters.split(',').forEach(item => {
         let [field, operator, value] = item.split('-');
         if(options.includes(field)) {
-            queryObj[field] = {[operator]: Number(value)}
+            queryObj[field] = {[operator]: Number(value)};
         }
     })
     }
