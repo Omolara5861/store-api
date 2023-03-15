d4c5838218da49f43b75bca3a811ba86cb1f1c3c
Omolara5861 <debbiegterra@gmail.com>
2023-03-15 17:35:49 +0100

    feat: :necktie: Add featured query method
    Create query method used to filter featured product

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,18 @@
+const product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('Custom errorCustom error');
-    // res.sendStatus(200).json({msg: 'Static Products Fetched'});
+    // throw new Error('Custom errorCustom error');
+    res.status(200).json({msg: 'Static Products Fetched'});
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'Products Fetched'});
+    const queryObj = {};
+    const {featured} = req.query;
+    if(featured) {
+        queryObj.featured = featured === 'true' ? true : false;
+    }
+    const products = await product.find(queryObj);
+    res.status(200).json({msg: 'Products Fetched Successfully', data: products, nbHits: products.length});
 }
 
 module.exports = {getAllProducts, getAllProductsStatic};
