d67f6e541a4e76cb423d2e3c76d2ef5848b69191
Omolara Adebowale <debbiegterra@gmail.com>
2023-03-15 19:58:19 +0100

    feat: :sparkles: Add sort functionality

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     const queryObj = {};
-    const {featured, company, name} = req.query;
+    const {featured, company, name, sort} = req.query;
     if(featured) {
         queryObj.featured = featured === 'true' ? true : false;
     }
@@ -18,7 +18,15 @@ const getAllProducts = async (req, res) => {
     if(name) {
         queryObj.name = {$regex: name, $options: 'i'}
     }
-    const products = await product.find(queryObj);
+    let result = product.find(queryObj);
+    if(sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    }
+    else {
+        result = result.sort('createdAt');
+    }
+    const products = await result;
     res.status(200).json({msg: 'Products Fetched Successfully', products, nbHits: products.length});
 }
 
