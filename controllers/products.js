a6c81d8a0f4c13f7460e944d748fe7222a8e45d8
Omolara Adebowale <debbiegterra@gmail.com>
2023-03-15 19:35:20 +0100

    feat: :necktie: Add query method to filter by product name
    Create a method that filters by product name using regex and is case insensitive. It checks if a product name includes the value pass in the name query

--- controllers/products.js
+++ controllers/products.js
@@ -8,12 +8,15 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
     const queryObj = {};
-    const {featured, company} = req.query;
+    const {featured, company, name} = req.query;
     if(featured) {
         queryObj.featured = featured === 'true' ? true : false;
     }
     if(company) {
         queryObj.company = company;
+    }
+    if(name) {
+        queryObj.name = {$regex: name, $options: 'i'}
     }
     const products = await product.find(queryObj);
     res.status(200).json({msg: 'Products Fetched Successfully', products, nbHits: products.length});
