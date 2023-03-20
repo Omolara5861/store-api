127b811b0317c6905c32fc1d16a671e68729e9f1
Omolara5861 <debbiegterra@gmail.com>
2023-03-20 10:09:39 +0100

    feat: :sparkles: Add route for the single method

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
-const { getAllProducts, getAllProductsStatic } = require('../controllers/products');
+const { getAllProducts, getAllProductsStatic, getProduct } = require('../controllers/products');
 const router = Router();
 
 router.route('/').get(getAllProducts);
 router.route('/static').get(getAllProductsStatic);
+router.route('/:id').get(getProduct);
 
 module.exports = router;
