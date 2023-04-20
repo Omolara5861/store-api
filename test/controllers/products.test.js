bb3f05dccb64f31a91f957fbb0ca050f873b48be
Omolara5861 <debbiegterra@gmail.com>
2023-04-20 14:04:13 +0100

    test: :white_check_mark: Setup test

--- test/controllers/products.test.js
+++ test/controllers/products.test.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose')
 const req = require('supertest')
 const app = require('../../src/app')
 
-beforeAll(async () => mongoose.connect(process.env.MONGODB_URI))
-
-describe('Store API Test', () => {
-  it('Returns 10 products from the database', async () => {
-    const res = await req(app).get('/')
-    expect(res.statusCode).toBe(200)
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
 })
 
-afterAll(async () => mongoose.disconnect(process.env.MONGODB_URI))
+afterAll(async () => await mongoose.connection.close())
