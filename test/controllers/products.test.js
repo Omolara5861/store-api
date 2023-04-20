90dfa5074ceb870df34294682a38ed3b61c94acb
Omolara5861 <debbiegterra@gmail.com>
2023-04-20 14:07:15 +0100

    test: :white_check_mark: Add test for getting products from database

--- test/controllers/products.test.js
+++ test/controllers/products.test.js
@@ -13,4 +13,12 @@ beforeAll(async () => {
   })
 })
 
+describe('Store API Test', () => {
+  it('Returns 10 products from the database', async () => {
+    const res = await req(app).get('/api/v1/products')
+    expect(res.statusCode).toBe(200)
+    expect(res.length).toBeGreaterThan(0)
+  })
+})
+
 afterAll(async () => await mongoose.connection.close())
